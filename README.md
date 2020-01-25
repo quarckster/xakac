6d0e6d94660b75613d36787ecd41addaa4d88a5b
Dmitry Misharov <quarckster@gmail.com>
2020-01-25 20:07:08 +0100

    Adding sources

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # xakac
-Forwards payloads to your local server
+This CLI utility was insipired by [smee.io client](https://github.com/probot/smee-client). It forwards
+payloads from remote servers to hosts in your network.
+
+## Usage
+
+`xakac -config /path/to/config.json`
+
+In `config.json` you can define as many source-target pairs as you want:
+
+```json
+[
+  {
+    "source": "https://source_url_1",
+    "target": "https://target_url_1"
+  },
+  {
+    "source": "https://source_url_2",
+    "target": "https://target_url_2"
+  }
+]
+```
+
+It's possible to specify sources and targets via environment variables:
+
+`XAKAC_SOURCE_TARGET_1=https://source_url_1,https://target_url_1`
+
+`XAKAC_SOURCE_TARGET_2=https://source_url_2,https://target_url_2`
+
+
+`xakac` establishes connections to each source via [event source](https://developer.mozilla.org/en-US/docs/Web/API/EventSource)
+and forwards payloads to a corresponding target.
---
+++ xakac.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+    "bytes"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+    "sync"
+    "time"
+
+    "github.com/donovanhide/eventsource"
+)
+
+type logWriter struct{}
+type Route struct {
+    Source string
+    Target string
+}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+    return fmt.Print(time.Now().UTC().Format("2006-01-02T15:04:05Z") + ": " + string(bytes))
+}
+
+func listenToChannel(source string, target string) {
+    stream, err := eventsource.Subscribe(source, "")
+    if err != nil {
+        log.Fatal(err)
+    }
+    for {
+        ev := <-stream.Events
+        if ev.Event() == "ready" {
+            log.Println("listening to", source)
+        }
+        if ev.Event() == "" {
+            deliverPayload(ev.Data(), source, target)
+        }
+    }
+}
+
+func deliverPayload(payload string, source string, target string) {
+    resp, err := http.Post(target, "application/json", bytes.NewBuffer([]byte(payload)))
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("payload from", source, "has been sent to", target, "status code", resp.StatusCode)
+}
+
+func parseEnviron() []Route {
+    var routes []Route
+    for _, envVar := range os.Environ() {
+        if strings.Contains(envVar, "XAKAC_SOURCE_TARGET_") {
+            pair := strings.Split(strings.Split(envVar, "=")[1], ",")
+            route := Route{pair[0], pair[1]}
+            routes = append(routes, route)
+        }
+    }
+    return routes
+}
+
+func parseConfig(path string) []Route {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    var routes []Route
+    err = json.Unmarshal(data, &routes)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return routes
+}
+
+func startListeners(routes []Route) {
+    var wg sync.WaitGroup
+    wg.Add(len(routes))
+    for _, route := range routes {
+        go listenToChannel(route.Source, route.Target)
+    }
+    wg.Wait()
+}
+
+func getRoutes() []Route {
+    configPathPtr := flag.String("config", "", "path to a config file in json format")
+    flag.Parse()
+    if *configPathPtr != "" {
+        return parseConfig(*configPathPtr)
+    }
+    return parseEnviron()
+}
+
+func main() {
+    log.SetFlags(0)
+    log.SetOutput(new(logWriter))
+    startListeners(getRoutes())
+}
